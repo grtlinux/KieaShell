be9f0948556819c91f7d44b8ea922d1b90ff078e
Kiea Seok Kang <grtlinux@gmail.com>
2018-11-10 04:27:30 +0900

    Update synergys.sh

---
+++ ver01/kiea-ubuntu18-01/synergys.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/synergys -f --no-tray --debug INFO --name kiea-ubuntu18 --log /home/kiea/.synergy/synergy.log -c /home/kiea/.synergy/synergy.conf --address :24800 > /dev/null 2>&1 &
+
+
